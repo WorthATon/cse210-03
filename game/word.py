b90d9f964babbfef4eda9d6d1020030604da86c0
WorthATon <11Worth4Me@gmail.com>
2022-10-17 17:04:38 -0400

    adding basic word.py file and moved jumper.py to game folder

---
+++ game/word.py
@@ -0,0 +1,24 @@
+class Word:
+    """The word to be guessed"""
+
+    def __init__(self):
+        """Constructs a Word
+
+        Args:
+            self (Word) An instance of Word
+        """
+        
+        self._word = self.draw_word()
+
+    def draw_word(self):
+        """ Gets a random word"""
+
+    def check_guess(self):
+        """ checks to see if guess is in word. 
+        If it is in the word then it displays place in word.
+        """
+
+    def is_guessed(self):
+        """ tells player that letter was already guessed.
+        """
+        
