ade9b38c1b4809d183fbd8ad66141d1d0c7f6cf4
Eduardo Serrano <eserrano@Eduardos-MacBook-Air.local>
2022-10-17 14:21:24 -0500

    Created main, terminal and player files

---
+++ game/player.py
@@ -0,0 +1,60 @@
+from game.terminal import Terminal
+
+class Player:
+    """The person that handles the game.
+    
+    The responsibility of a Player is to control the sequence of play.
+    
+    Attributes:
+        _word_to_guess (Word): The word the player has to guess
+        _jumper (Jumper): The games jumper
+        _terminal (Terminal): For getting and displaying information on the terminal.
+    """
+    
+    def __init__(self):
+        """Constructs a new Player.
+        
+        Args:
+            self (Player): and instance of Player
+        """
+
+        self._word_to_guess = ""
+        self._jumper = ""
+        self._terminal = Terminal()
+        
+        
+    def start_game(self):
+        """Starts the game by running the game loop
+        
+        Args:
+            self (Player): an instance of Player
+        """
+        
+        while self._jumper.is_alive():
+            self._get_inputs()
+            self._do_updates()
+            self._do_outputs()
+        
+    def _get_inputs(self):
+        """Makes a guess for the word.
+        
+        Args:
+            self (Player): An instance of Player
+        """
+        pass
+    
+    def _do_updates(self):
+        """Determines if the Jumper has any lives left or if the word has been guessed.
+        
+        Args: 
+            self (Player): An instance of Player
+        """
+        pass
+    
+    def _do_outputs(self):
+        """Displays the Jumper's state and the word status.
+        
+        Args:
+            self (Player): an instance of Player
+        """
+        pass
